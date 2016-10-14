994505e93c05495c6848788f8a3cec894ed5967c
Catherine Liu <catherineliu2102@gmail.com>
2016-10-14 06:49:18 -0400

    Update plot_wdi.R

--- R/plot_wdi.R
+++ R/plot_wdi.R
@@ -9,8 +9,19 @@
 #'
 
 plot_wdi <- function(wdi_df){
-
-  p <- ggplot(data = wdi_df, aes(x = date, y = value, group = country)) + geom_line()
-
-  return(p)
+  
+wdi_df$value = wdi_df$value/1000000
+  
+p <- ggplot(data = wdi_df, aes(x = date, y = value, group = country)) + geom_line(aes(colour = value))
+  
+p <- p + scale_x_continuous(breaks = seq(1960, 2015, 5))
+  
+p <- p + scale_y_continuous(breaks = seq(0, 1500, 300))
+  
+p <- p + labs(title = wdi_df$indicatorID[1])
+  
+p <- p + ylab("value (in millions)") + xlab("Year")
+  
+return(p)
+  
 }
