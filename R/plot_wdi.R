8cb22466757651d1d410d8f6e945b2a04d8eefe0
Catherine Liu <catherineliu2102@gmail.com>
2016-10-14 14:56:22 -0400

    Update plot_wdi.R

--- R/plot_wdi.R
+++ R/plot_wdi.R
@@ -9,8 +9,16 @@
 #'
 
 plot_wdi <- function(wdi_df){
+  
+  wdi <- unique(wdi_df$indicator)
 
-  p <- ggplot(data = wdi_df, aes(x = date, y = value, group = country)) + geom_line()
-
-  return(p)
+  p <- ggplot(data = wdi_df, aes_(x = ~date, y = ~value, group = ~country)) + 
+    geom_line(color = 'blue', alpha = .8, size = rel(0.9), aes_(text = ~country))
+  p <- p + ylab(wdi)
+  p <- p + ggthemes::theme_hc()
+  p <- p + scale_x_continuous(breaks = c(1960, 1970, 1980, 1990, 2000, 2010))
+  p <- p + scale_y_continuous(labels = scales::comma)
+  p <- p + ggtitle(paste0('Evolution of ', wdi, ' over time\nEach line represents a country\n'))
+  
+  plotly::ggplotly(p, tooltip = 'text')
 }
